ad60ceb139fec88f5ee60f7c13903440280e12dc
Debashis Roy <goodquote39@hotmail.com>
2023-03-31 01:49:17 +0600

    Home page configuring

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
-      <div className="flex lg:flex-row md:flex-row sm:flex-col mi-[320px]:flex-col p-20 bg-amber-600 m-10">
-        <div className="w-1/2 p-3">
+      <div className="flex lg:flex-row md:flex-row sm:flex-col min-[320px]:flex-col lg:p-20 sm:p-2 min-[320px]:p-1 bg-amber-600 m-10">
+        <div className="lg:w-1/2 sm:w-[95%] min-[320px]:w-[95%] p-3">
           <div className="border-2 p-4 rounded-lg">
             {/* <img src={ } alt=''/> */}
             images
           </div>
         </div>
-        <div className="w-1/2 p-3">
+        <div className="lg:w-1/2 sm:w-[95%] min-[320px]:w-[95%] p-3">
           <div className="border-2 p-4 rounded-lg">
             <h3 className="my-10">Need to making some cocktails.. </h3>
             <Link
