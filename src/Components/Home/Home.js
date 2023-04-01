823c20213089b96b462658150d319e12feb022c7
Debashis Roy <goodquote39@hotmail.com>
2023-04-02 00:29:43 +0600

    home page background added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -15,9 +15,12 @@ const Home = () => {
 
   console.log(item);
   return (
-    <div className="flex lg:flex-row md:flex-row sm:flex-col min-[320px]:flex-col lg:p-20 sm:p-2 min-[320px]:p-1 bg-amber-600 m-10 items-center">
+    <div
+      id="homepage"
+      className="flex lg:flex-row md:flex-row sm:flex-col min-[320px]:flex-col lg:p-20 sm:p-2 min-[320px]:p-1 bg-amber-600 m-10 items-center"
+    >
       <div className="lg:w-1/2 sm:w-[95%] min-[320px]:w-[95%] p-3 ">
-        <div className="border-2 p-4 rounded-lg bg-indigo-400">
+        <div className="border-2 p-4 rounded-lg bg-indigo-600 shadow-inner shadow-black">
           <img
             src={item?.strDrinkThumb}
             alt="image"
@@ -25,10 +28,12 @@ const Home = () => {
           <p className="text-white font-semibold">{item?.strDrink}</p>
         </div>
       </div>
-      <div className="lg:w-1/2 sm:w-[95%] min-[320px]:w-[95%] p-3">
-        <h2 className="text-3xl font-bold my-5">Welcome !!</h2>
+      <div className="lg:w-1/2 sm:w-[95%] min-[320px]:w-[95%] p-3 bg-black opacity-90 rounded-lg">
+        <h2 className="text-3xl font-bold my-5 text-white">Welcome !!</h2>
         <div className="border-2 p-4 rounded-lg">
-          <h3 className="my-10 text-xl">Want some cocktails & drinks ..? </h3>
+          <h3 className="my-10 text-xl text-white">
+            Want some cocktails & drinks ..?{" "}
+          </h3>
           <Link
             className="bg-lime-200 p-3 rounded-lg hover:bg-lime-400 hover:shadow-md hover:shadow-black"
             to="/cocktails"
